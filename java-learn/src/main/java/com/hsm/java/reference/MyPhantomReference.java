bf839925d081e51b6e3de1971b46f3e31693ec21
黄森明 <1586558083@qq.com>
2021-03-01 18:58:59 +0800

    引用

--- java-learn/src/main/java/com/hsm/java/reference/MyPhantomReference.java
+++ java-learn/src/main/java/com/hsm/java/reference/MyPhantomReference.java
@@ -1,5 +1,9 @@
 package com.hsm.java.reference;
 
+import com.hsm.java.util.ThreadUtils;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
 /**
  * @Classname MyPhantomReference
  * @Description 虚引用
@@ -7,4 +11,24 @@ package com.hsm.java.reference;
  * @Created by senming.huang
  */
 public class MyPhantomReference {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+        PhantomReference<Object> phantomReference = new PhantomReference<>(o1,referenceQueue);
+
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+
+        System.out.println("==============================");
+        o1 = null;
+        System.gc();
+        ThreadUtils.sleep(1000);
+
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+
+    }
 }
