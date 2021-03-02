96001c034592fa4da649aaacbe30116dbd824143
Steven-hsm <1586558083@qq.com>
2021-03-02 22:08:01 +0800

    aqs

--- java-learn/src/main/java/com/hsm/java/aqs/AQSDemo.java
+++ java-learn/src/main/java/com/hsm/java/aqs/AQSDemo.java
@@ -11,8 +11,10 @@ import java.util.concurrent.locks.ReentrantLock;
 public class AQSDemo {
 
     public static void main(String[] args) {
-        ReentrantLock ReentrantLock = new ReentrantLock();
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
 
+        lock.unlock();
 
     }
 }
