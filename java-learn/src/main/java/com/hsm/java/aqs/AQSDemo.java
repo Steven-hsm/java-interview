0c39c9698b96e3ce83e55ffb137a1c4eda8a9334
黄森明 <1586558083@qq.com>
2021-03-02 21:29:12 +0800

    引用

---
+++ java-learn/src/main/java/com/hsm/java/aqs/AQSDemo.java
@@ -0,0 +1,18 @@
+package com.hsm.java.aqs;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Classname AQSDemo
+ * @Description aqs
+ * @Date 2021/3/2 11:10
+ * @Created by senming.huang
+ */
+public class AQSDemo {
+
+    public static void main(String[] args) {
+        ReentrantLock ReentrantLock = new ReentrantLock();
+
+
+    }
+}
