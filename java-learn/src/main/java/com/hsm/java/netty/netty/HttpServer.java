814f0e4f05f4a66db33a6d789c9fad2b46fec4e4
黄森明 <huangsenming@yjzhixue.com>
2021-08-06 09:05:57 +0800

    TCP服务

---
+++ java-learn/src/main/java/com/hsm/java/netty/netty/HttpServer.java
@@ -0,0 +1,36 @@
+package com.hsm.java.netty.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @Classname HttpServer
+ * @Description TODO
+ * @Date 2021/8/5 20:59
+ * @Created by huangsm
+ */
+public class HttpServer {
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(6668).sync();
+
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
