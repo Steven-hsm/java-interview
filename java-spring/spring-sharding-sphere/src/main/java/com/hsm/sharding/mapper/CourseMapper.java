ef0de0dbd81827e27def5e4d569e633afdf3ec9e
Steven-hsm <1586558083@qq.com>
2021-12-16 10:59:41 +0800

    shardingjdbc

---
+++ java-spring/spring-sharding-sphere/src/main/java/com/hsm/sharding/mapper/CourseMapper.java
@@ -0,0 +1,14 @@
+package com.hsm.sharding.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hsm.sharding.entity.Course;
+
+/**
+ * @author ：楼兰
+ * @date ：Created in 2021/1/4
+ * @description:
+ **/
+
+public interface CourseMapper extends BaseMapper<Course> {
+}
---
+++ java-spring/spring-sharding-sphere/src/main/java/com/hsm/sharding/mapper/DictMapper.java
@@ -0,0 +1,13 @@
+package com.hsm.sharding.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hsm.sharding.entity.Dict;
+
+/**
+ * @author ：楼兰
+ * @date ：Created in 2021/1/5
+ * @description:
+ **/
+public interface DictMapper extends BaseMapper<Dict> {
+}
