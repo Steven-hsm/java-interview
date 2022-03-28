1e4ecf79c3c4823916ce3a8848dce6931099946b
Steven-hsm <1586558083@qq.com>
2022-03-28 21:03:22 +0800

    protobuf

--- java-learn/src/main/java/com/hsm/java/serialize/protobuf/Main.java
+++ java-learn/src/main/java/com/hsm/java/serialize/protobuf/Main.java
@@ -1,5 +1,7 @@
 package com.hsm.java.serialize.protobuf;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+
 /**
  * Copyright © 2017 - 2022 深圳鲲鹏云智教育科技有限公司
  * TODO
@@ -9,9 +11,14 @@ package com.hsm.java.serialize.protobuf;
  * @date 2022/3/28 20:47
  */
 public class Main {
-    public static void main(String[] args) {
-        NettyMessage.NettyMessageProToBuff.Builder builder = new NettyMessage.NettyMessageProToBuff.Builder();
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        NettyMessage.NettyMessageProToBuff.Builder builder = NettyMessage.NettyMessageProToBuff.newBuilder();
         builder.setRequestId("1231");
+        builder.setMsgType(1);
+        builder.setData("1231");
+        NettyMessage.NettyMessageProToBuff build = builder.build();
+        NettyMessage.NettyMessageProToBuff nettyMessageProToBuff = NettyMessage.NettyMessageProToBuff.parseFrom(build.toByteArray());
+
 
     }
 
