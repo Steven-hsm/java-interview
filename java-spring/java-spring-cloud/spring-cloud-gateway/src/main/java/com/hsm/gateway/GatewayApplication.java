77e9b5676d887947548aee7eca857120a78aec4c
1586558083@qq.com <1586558083@qq.com>
2024-10-08 23:08:31 +0800

    spring cloud gateway集成

--- java-spring/java-spring-cloud/spring-cloud-gateway/src/main/java/com/hsm/gateway/GatewayApplication.java
+++ java-spring/java-spring-cloud/spring-cloud-gateway/src/main/java/com/hsm/gateway/GatewayApplication.java
@@ -3,6 +3,11 @@ package com.hsm.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 参考文档
+ * 1. https://www.bilibili.com/video/BV1PN4y1s7UB
+ * 2. https://blog.csdn.net/qq_34417433/article/details/135144279
+ */
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
